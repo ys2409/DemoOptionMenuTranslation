3212f119445260004ade145dd1aa33c5b7de20a2
ys2409 <19022397@myrp.edu.sg>
2020-06-02 15:31:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19022397/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id19022397.demooptionmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+}
